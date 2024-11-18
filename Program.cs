fe6dea2aa93d3f63f6f03a50aabe40d8b873f6ce
rilez366 <rilez366@outlook.com>
2024-11-18 16:42:03 +0000

    added the swagger stuff and the API and then changed it to suit our project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo {
+         Title = "CookNest API",
+         Description = "Creating and delivering the recipes you need",
+         Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "CookNest API V1");
+   });
+}
 
 app.MapGet("/", () => "Hello World!");
 
