5d4e3ec14b0a0ecbaacd5e33495dfe8425d61354
Danielle <daniellelee73@gmail.com>
2024-11-18 16:40:11 +0000

    Third Commit to connect back to front

--- Program.cs
+++ Program.cs
@@ -29,14 +29,5 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 
-// app.MapPost("/signup", (User user) => 
-// {
-//     return UserDB.CreatneUser(user);
-// });
-
-// app.MapPost("/login", (LoginRequest loginRequest) => 
-// {
-//     return UserDB.AuthenticateUser(loginRequest);
-// });
 
 app.Run();
